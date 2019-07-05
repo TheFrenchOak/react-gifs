16d85fc714f39781d2b797e1999e2fdef0082a07
Frederic Laffont <frederic_laffont@msn.com>
2019-07-05 19:04:55 +0200

    complete giphy app

---
+++ src/components/app.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "etn52ENYVnpxqMaXiT"
+    };
+  }
+
+  search = (query) => {
+    giphy('KwKDwfkOrEd4FqghPh99NPzttSzRxH4M').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+    const src = `https://media.giphy.com/media/${this.props.id}/200w.gif`;
+    return (
+      <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
